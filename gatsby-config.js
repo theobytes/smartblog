94774b3a1c7d6de06d63626f4363e8d4bbfe3b45
theophilus <tdbvume@outlook.com>
2020-01-12 18:09:59 +0200

    smartblog new

---
+++ gatsby-config.js
@@ -0,0 +1,109 @@
+module.exports = {
+  siteMetadata: {
+    // edit below
+    title: `Sharing Tech Ideas & Knowledge`,
+    author: `Theophilus Ngaribvume`,
+    description: `A blog to share technology, programming (coding) knowledge and love`,
+    siteUrl: `https://theophilus.co`,
+    social: {
+      twitter: `clouditate`,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-feed-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-vscode`,
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          },
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131251832-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "GTM-MJD34BD",
+        // Enables Google Optimize Experiment ID
+        experimentId: "hK_K1BH2ScWjYeMlAOsinw",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "0",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "theophilus.co",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        // edit below
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+
+class IndexPage extends React.Component {
+  render() {
+    const siteTitle = "Theophilus Ngaribvume Tech Blog"
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Home"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <img style={{ margin: 0 }} src="./blog.svg" alt={siteTitle} />
+        <h1>
+          Hey people{" "}
+          <span role="img" aria-label="wave emoji">
+            👋
+          </span>
+        </h1>
+        <p>Welcome to my new all tech blog. Enjoy.</p>
+        <p>
+          Technology is love, empowering our generation to reach new heights and to hope for a better future, From the internet to artificial intelligence to embedded devices, technology conquered our lives.
+        </p>
+        <p>Now a new wave of technology <span role="img" aria-label="wave emoji">👋</span>RPA(Robotic Process Automation),<span role="img" aria-label="wave emoji">👋</span>Blockchain, <span role="img" aria-label="wave emoji">👋</span>Cognitive Cloud Computing, <span role="img" aria-label="wave emoji">👋</span>Internet of Things, <span role="img" aria-label="wave emoji">👋</span>Autonomous cars, <span role="img" aria-label="wave emoji">👋</span>Flying Cars and many others will seek to disrupt our ways of living</p>
+        <p>Let's read some stories!</p>
+        <Link to="/blog/">
+          <Button marginTop="35px">Go to Blog</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
